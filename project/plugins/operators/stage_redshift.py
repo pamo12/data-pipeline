098235be57cf46dd399a3d319329d65aacd468a6
Patricia Montag <post@patricia-montag.de>
2020-01-10 15:01:07 +0100

    Implement operator to stage data from S3 to Redshift

--- project/plugins/operators/stage_redshift.py
+++ project/plugins/operators/stage_redshift.py
@@ -1,24 +1,49 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
-
+    sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        FORMAT AS JSON '{}'
+    """
+    
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id,
+                 aws_credentials_id,
+                 target_table,
+                 s3_bucket,
+                 s3_key,
+                 json_path,
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.target_table = target_table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+        formatted_sql = StageToRedshiftOperator.sql.format(
+            self.target_table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.json_path
+        )
+        redshift.run(formatted_sql)
 
 
 
